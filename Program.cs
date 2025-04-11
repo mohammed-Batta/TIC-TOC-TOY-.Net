d7e5dbce8b615405962544817fa6be5ba4fb2296
EL BATTA MOHAMED <114107925+mohammed-Batta@users.noreply.github.com>
2025-04-11 17:08:18 +0200

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+    ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +16,7 @@ namespace MyFirstWinFormsProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MyTicTacSoutoion());
+            Application.Run(new HisTicTac());
 
         }
     }
